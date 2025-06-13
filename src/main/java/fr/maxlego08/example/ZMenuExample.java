2ee30cc99bade3a1f575de1d9a36d2f920b4dddc
Maxlego08 <maxlego08@sfr.fr>
2025-06-13 18:47:14 +0200

    :tada: Rework zmenu api 1.1.0.0

---
+++ src/main/java/fr/maxlego08/example/ZMenuExample.java
@@ -0,0 +1,17 @@
+package fr.maxlego08.example;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZMenuExample extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
